37bc29727b72fde0c591d7e836739f242e8ac337
Ivan Paljak <ipaljak@gmail.com>
2020-07-12 00:22:24 +0200

    Validator za skandaloznog

---
+++ probno/skandalozni/validate3.py
@@ -0,0 +1,94 @@
+
+"""Upotreba ./validate3.py test/*.in*"""
+
+import string
+
+
+def check(lines):
+    nl = []   # ispravno formatirane linije
+    E = "\n"  # line ending
+
+    n = lines[0].strip()
+    assert len(n) <= 1000001, "n izvan intervala"
+    if len(n) == 1000001:
+        assert n[0] == '1', "n izvan intervala"
+        for c in n[1:]:
+            assert c == '0', "n izvan intervala"
+    nl.append("{}{}".format(n, E));
+
+    assert lines == nl, "Krivi format (%s vs %s)" % (lines, nl)
+    assert lines[-1][-1] == "\n", "Zadnji red ne zavrsava sa \\n"
+    return {'n': n}
+
+
+# Ocekivani clusteri! Ovo vjerojatno zelis promijeniti!
+expected_clusters = {"Subtask 1": 1, "Subtask 2": 1, "Subtask 3": 1, "Subtask 4": 1}
+
+
+def what_cluster(data):
+    # na temelju povratne informacije iz check(lines)
+    # zakljucuje za TP u kojoj je bodovnoj sekciji
+    if len(data['n']) > 7: return "Subtask 4"
+    if int(data['n']) <= 50: return "Subtask 1"
+    if int(data['n']) <= 1000: return "Subtask 2"
+    if int(data['n']) <= 1000000: return "Subtask 3"
+    return "Impossible"
+
+################### Zadatak-specifican kod iznad ove linije #########################
+
+import sys
+import glob
+import hashlib
+
+
+def group_in_batches(files):
+    # mnozenje.in.1a, mnozenje.in.1b sprema u isti batch
+
+    files.sort()
+    B = []
+    for f in files:
+        if f[-1].islower() and len(B) > 0 and f[:-1] == B[-1][-1][:-1]:
+            B[-1].append(f)
+        else:
+            B.append([f])
+    return B
+
+
+if __name__ == "__main__":
+    f = []
+    for pattern in sys.argv[1:]:
+        for filename in glob.glob(pattern):
+            f.append(filename)
+
+    bc = []
+    for batch in group_in_batches(f):
+        if 'dummy' not in batch[0]:
+            bc.append([])
+        for filename in batch:
+            print("{}: ".format(filename), end="")
+            try:
+                lines = open(filename).readlines()
+                summary = check(lines)
+                c = what_cluster(summary)
+                if 'dummy' not in batch[0]:
+                    bc[-1].append(c)
+            except Exception as e:
+                print("Greska!", e)
+                raise
+            else:
+                summary['n'] = len(summary['n'])
+                print("Sve ok! (cluster {}, summary = {})".format(c, summary))
+
+    clusters = {}
+    for b in bc:
+        # for c in b:
+            # assert c == b[0], "Ima razlicitih cluster-a unutar batcha"
+        if not b[0] in clusters:
+            clusters[b[0]] = 0
+        clusters[b[0]] += 1
+
+    # assert clusters == expected_clusters, "Kriva raspodjela clustera ({} vs {})".format(clusters, expected_clusters)
+
+    # Buda test - provjeri duplikate
+    hashes = set(hashlib.sha1(open(x, 'rb').read()).hexdigest() for x in f)
+    # assert len(hashes) == len(f), "Ima duplikata!"
