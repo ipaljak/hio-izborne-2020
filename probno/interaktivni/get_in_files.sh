935d86f9216377b34e7505c1421e4448737ed5b2
Marin <marinkisic14@gmail.com>
2020-07-13 17:01:05 +0200

    dodano jos par primjera

--- probno/interaktivni/get_in_files.sh
+++ probno/interaktivni/get_in_files.sh
@@ -3,6 +3,8 @@
 ./gen 123 4 4 81 > test/interaktivni.in.1c
 ./gen 42 4 4 81 > test/interaktivni.in.1d
 ./gen 98 4 4 81 > test/interaktivni.in.1e
+./gen 98 4 4 99 > test/interaktivni.in.1f
+./gen 98 4 4 97 > test/interaktivni.in.1g
 
 ./gen 42 1 100000 90 > test/interaktivni.in.2a
 ./gen 32 23 424 78 > test/interaktivni.in.2b
@@ -11,3 +13,6 @@
 ./gen 32 300 300 69 > test/interaktivni.in.2e
 ./gen 31 50 40 87 > test/interaktivni.in.2f
 ./gen 42 500 200 99 > test/interaktivni.in.2g
+./gen 41 20 5000 96 > test/interaktivni.in.2h
+./gen 44 1000 100 97 > test/interaktivni.in.2i
+./gen 49 500 200 95 > test/interaktivni.in.2j
