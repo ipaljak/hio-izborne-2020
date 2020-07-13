0b5fefa967a86a78e64fdfbbc30f4a403cf3093a
Ivan Paljak <ipaljak@gmail.com>
2020-07-13 21:09:30 +0200

    Blago popravljen tester na interaktivnom

--- probno/interaktivni/tester.sh
+++ probno/interaktivni/tester.sh
@@ -2,6 +2,8 @@ for f in test/*.in*;
 do
   echo "-------------------------------------------------"
   echo $f
+  mkfifo temp
   ./interaktivni_server $f flog foutput < temp | ./marin > temp
   cat foutput
+  rm temp
 done
