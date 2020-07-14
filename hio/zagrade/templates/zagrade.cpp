682203e46d62b14e8152b62b1d457672a7919be5
Paula <paulavidas@gmail.com>
2020-07-14 19:56:29 +0200

    sample kodovi za zagrade

---
+++ hio/zagrade/templates/zagrade.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, odgovor;
+    cin >> n;
+
+    cout << "? 1 6" << endl;
+    cout.flush();
+    cin >> odgovor;
+
+    cout << "? 1 2" << endl;
+    cout.flush();
+    cin >> odgovor;
+
+    cout << "? 2 4" << endl;
+    cout.flush();
+    cin >> odgovor;
+
+    cout << "? 2 5" << endl;
+    cout.flush();
+    cin >> odgovor;
+
+    cout << "? 3 4" << endl;
+    cout.flush();
+    cin >> odgovor;
+
+    cout << "! ((()))" << endl;
+    cout.flush();
+
+    return 0;
+}
