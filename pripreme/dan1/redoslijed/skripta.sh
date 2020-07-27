05ead74bbe19052e6f737ab01d6093acef7e775d
Adrian Beker <adro.beker@gmail.com>
2020-07-27 20:01:49 +0200

    updateana skripta

--- pripreme/dan1/redoslijed/skripta.sh
+++ pripreme/dan1/redoslijed/skripta.sh
@@ -1,9 +1,9 @@
-# Usage: bash skripta.sh N M L gen sol1 sol2
-# with executable {{checker}} in path
+# Usage: bash skripta.sh N M L type sol1 sol2
+# with executables {{checker}}, {{generator}} in path
 cnt=0
 while true; do
     echo Generating testcase with N=$1, M=$2...
-    ./$4 $1 $2 $3 > inp
+    ./generator $1 $2 $3 $4 > inp
     echo Running $5...
     time ./$5 < inp > off
     echo Running $6...
