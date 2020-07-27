4f79f4b0116bddd79932c0c387f87bea04b4e83d
Paula <paulavidas@gmail.com>
2020-07-28 00:21:10 +0200

    poboljsano random rj za sadnice

--- pripreme/dan1/sadnice/paula_random_stablo.cpp
+++ pripreme/dan1/sadnice/paula_random_stablo.cpp
@@ -12,7 +12,7 @@ typedef long double ld;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int n, m;
+int n, m, N, M;
 vector<string> sol;
 
 void initialize() {
@@ -41,6 +41,21 @@ void print() {
     for (auto& row : sol) cout << row << '\n';
 }
 
+int score() {
+    int score = 0;
+    for (int i = 1; i < N; i += 2) {
+        int cnt = 0;
+        for (int j = 0; j < M; j += 3) cnt += sol[i][j] == '|';
+        score = max(score, cnt);
+    }
+    for (int j = 1; j < M; j += 3) {
+        int cnt = 0;
+        for (int i = 0; i < N; i += 2) cnt += sol[i][j] == '-';
+        score = max(score, cnt);
+    }
+    return score;
+}
+
 int ceil(int a, int b) { return (a - 1) / b + 1; }
 
 int main() {
@@ -51,44 +66,57 @@ int main() {
     srand(time(0));
 
     cin >> n >> m;
+    N = 2 * n + 1, M = 3 * m + 1;
 
-    initialize();
+    vector<string> best;
+    int best_score = 1e9;
 
-    vector<pair<pii, pii>> E;
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= m; j++) {
-            if (i != n) E.push_back({{i, j}, {i + 1, j}});
-            if (j != m) E.push_back({{i, j}, {i, j + 1}});
+    for (int iter = 0; iter < 1e6 / (n * m); iter++) {
+        initialize();
+
+        vector<pair<pii, pii>> E;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i != n) E.push_back({{i, j}, {i + 1, j}});
+                if (j != m) E.push_back({{i, j}, {i, j + 1}});
+            }
+        }
+        random_shuffle(E.begin(), E.end());
+
+        vi p((n + 1) * (m + 1));
+        iota(p.begin(), p.end(), 0);
+        for (auto& e : E) {
+            int a = e.fi.fi * (m + 1) + e.fi.se, u = a;
+            int b = e.se.fi * (m + 1) + e.se.se, v = b;
+
+            while (p[a] != a) a = p[a];
+            while (p[b] != b) b = p[b];
+            while (u != a) {
+                int t = u;
+                u = p[t];
+                p[t] = a;
+            }
+            while (v != b) {
+                int t = v;
+                v = p[t];
+                p[t] = b;
+            }
+
+            if (a != b) {
+                add_edge(e);
+                if (rand() & 1) swap(a, b);
+                p[a] = b;
+            }
+        }
+
+        int cur_score = score();
+        if (cur_score < best_score) {
+            best_score = cur_score;
+            best = sol;
         }
     }
-    random_shuffle(E.begin(), E.end());
-    
-    vi p((n + 1) * (m + 1));
-    iota(p.begin(), p.end(), 0);
-    for (auto& e : E) {
-        int a = e.fi.fi * (m + 1) + e.fi.se, u = a;
-        int b = e.se.fi * (m + 1) + e.se.se, v = b;
-    
-        while (p[a] != a) a = p[a];
-        while (p[b] != b) b = p[b];
-        while (u != a) {
-            int t = u;
-            u = p[t];
-            p[t] = a;
-        }
-        while (v != b) {
-            int t = v;
-            v = p[t];
-            p[t] = b;
-        }
 
-        if (a != b) {
-            add_edge(e);
-            if (rand() & 1) swap(a, b);
-            p[a] = b;
-        }
-    }
-
+    sol = best;
     print();
 
     return 0;
