c6779cff5f1a8fd4f3f7a1c264f8299b20a66155
Adrian Beker <adro.beker@gmail.com>
2020-07-28 18:35:17 +0200

    skripta za pokretanje na sluzbenim primjerima

--- pripreme/dan1/redoslijed/run.sh
+++ pripreme/dan1/redoslijed/run.sh
@@ -1,1 +1,34 @@
-./$1 < test/redoslijed.in.$2 > test/redoslijed.out.$2
+for i in {1..3}
+do
+  ./redoslijed_minmax < test/redoslijed.dummy.in.$i > test/redoslijed.dummy.out.$i
+done
+
+for i in {a..d}
+do
+  ./redoslijed_minmax < test/redoslijed.in.1$i > test/redoslijed.out.1$i
+done
+
+for i in {a..f}
+do
+  ./redoslijed_minmax < test/redoslijed.in.2$i > test/redoslijed.out.2$i
+done
+
+for i in {a..l}
+do
+  ./redoslijed_minmax < test/redoslijed.in.3$i > test/redoslijed.out.3$i
+done
+
+for i in {a..h}
+do
+  ./redoslijed_minmax < test/redoslijed.in.4$i > test/redoslijed.out.4$i
+done
+
+for i in {a..j}
+do
+  ./redoslijed_minmax < test/redoslijed.in.5$i > test/redoslijed.out.5$i
+done
+
+for i in {a..o}
+do
+  ./redoslijed_minmax < test/redoslijed.in.6$i > test/redoslijed.out.6$i
+done
