e2e5782b56e0742c3ba74e0f7617bdd4f77458c1
Tonko Sabolcec <tonkosi123@gmail.com>
2020-07-30 00:31:43 +0200

    updejt generatori za restoran

---
+++ pripreme/dan2/restoran/gen.sh
@@ -0,0 +1,13 @@
+TASK=restoran
+
+# Subtask 1
+./gen 29834 8 1 1 > test/$TASK.in.1a
+./gen 23389 9 1 1 > test/$TASK.in.1b
+./gen 84989 9 1 1 > test/$TASK.in.1c
+./gen 58489 9 1 1 > test/$TASK.in.1d
+./gen 89712 9 1 1 7 > test/$TASK.in.1e
+./gen 89057 7 1 1 5 > test/$TASK.in.1f
+./gen 83495 8 1 1 3 > test/$TASK.in.1g
+./gen_spec 94239 9 1 1 > test/$TASK.in.1h
+
+
--- pripreme/dan2/restoran/gen_spec.cpp
+++ pripreme/dan2/restoran/gen_spec.cpp
@@ -1,5 +1,5 @@
 // Koristenje:
-// ./gen $RANDOM n k [poredak da/ne (bool 0/1, default 1)] [max_a,b, default 1e9]
+// ./gen seed n k [poredak da/ne (bool 0/1, default 1)]
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,12 +11,23 @@ int main(int argc, char **argv) {
   int k = atoi(argv[3]);
   bool provjera = true;
   if (argc > 4) provjera = (bool)atoi(argv[4]);
-  int maxval = 1000000000;
-  if (argc > 5) maxval = atoi(argv[5]);
+  int maxval = 10000000;
 
   cout << n << " " << k << endl;
+  vector<pair<int, int>> vvv;
+
+  auto randval = [maxval](){
+    return 20000 + rand() % (maxval - 2*20000);
+  };
+
   for (int i = 0; i < n; ++i) {
-    cout << 1 + rand() % maxval << " " << 1 + rand() % maxval << endl;
+    if (i < n/2) vvv.push_back({2*i+1, 2*i+2});
+    else vvv.push_back({maxval-2*i, maxval-2*i-1});
+  }
+  
+  random_shuffle(vvv.begin(), vvv.end());
+  for (auto it : vvv) {
+    cout << it.first << " " << it.second << endl;
   }
 
   set<int> active;
@@ -25,7 +36,7 @@ int main(int argc, char **argv) {
   
   int poretci = 0;
   if (provjera && k > 0) {
-    poretci = 1 + rand() % min(n / 4, min(k, 10));
+    poretci = 1 + rand() % (1+min(n / 4, min(k, 10)));
   }
 
   vector<int> mask(k, 0);
@@ -40,7 +51,7 @@ int main(int argc, char **argv) {
       continue;
     }
     if (active.empty() || rand() % 2) {
-      cout << "DOLAZI " << 1 + rand() % maxval << " " << 1 + rand() % maxval << endl;
+      cout << "DOLAZI " << randval() << " " << randval() << endl;
       active.insert(next_id++);
     } else {
       auto it = active.lower_bound(rand() % next_id);
