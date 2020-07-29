f9714f8ee6dbed58a666ebe6070f5a0432cf6b44
Ivan Paljak <ipaljak@gmail.com>
2020-07-29 21:04:56 +0200

    Prvi subtask na restoranu

--- pripreme/dan2/restoran/paljak_subtask1.cpp
+++ pripreme/dan2/restoran/paljak_subtask1.cpp
@@ -9,14 +9,16 @@ using namespace std;
 
 typedef long long llint;
 
-int n, q;
-vector<pair<int, int>> v;
+using Guest = pair<int, pair<int, int>>;
+
+int n, k;
+vector<Guest> v, order;
 
 llint get_time() {
   llint eat_queue = 0;
   llint total = 0;
   for (const auto &p : v) {
-    int a = p.first, b = p.second;
+    int a = p.second.first, b = p.second.second;
     eat_queue = max(0LL, eat_queue - (llint) a);
     total += a;
     eat_queue += (llint) b;
@@ -24,23 +26,30 @@ llint get_time() {
   return total + eat_queue;
 }
 
-int main(void) {
-  scanf("%d%d", &n, &q);
+  int main(void) {
+  scanf("%d%d", &n, &k);
   for (int i = 0; i < n; ++i) {
     int a, b;
     scanf("%d%d", &a, &b);
-    v.emplace_back(a, b);
+    v.push_back({i, {a, b}});
   }
 
-  assert(q == 0);
+  assert(k == 1);
 
   sort(v.begin(), v.end());
   llint sol = 1e18;
   do {
-    sol = min(sol, get_time());
+    int t = get_time();
+    if (t < sol) {
+      sol = t;
+      order = v;
+    }
   } while (next_permutation(v.begin(), v.end()));
 
   printf("%lld\n", sol);
+  for (const auto &p : order) printf("%d ", p.first + 1);
+  for (const auto &p : order) printf("%d ", p.first + 1);
+  printf("\n");
 
   return 0;
 }
