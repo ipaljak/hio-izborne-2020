92000ab312724a4a8daef3a1c3811963da228ae4
Ivan Paljak <ipaljak@gmail.com>
2020-07-30 01:10:02 +0200

    Generator i primjeri za restoran

--- pripreme/dan2/restoran/gen.sh
+++ pripreme/dan2/restoran/gen.sh
@@ -8,6 +8,74 @@ TASK=restoran
 ./gen 89712 9 1 1 7 > test/$TASK.in.1e
 ./gen 89057 7 1 1 5 > test/$TASK.in.1f
 ./gen 83495 8 1 1 3 > test/$TASK.in.1g
-./gen_spec 94239 9 1 1 > test/$TASK.in.1h
+./gen_spec 94239 7 1 1 > test/$TASK.in.1h
+./gen_spec 43241 8 1 1 > test/$TASK.in.1i
+./gen_spec 32134 9 1 1 > test/$TASK.in.1i
 
+# Subtask 2
+./gen 29834 11 1 1 > test/$TASK.in.2a
+./gen 23389 15 1 1 > test/$TASK.in.2b
+./gen 84989 20 1 1 > test/$TASK.in.2c
+./gen 58489 20 1 1 > test/$TASK.in.2d
+./gen 43423 20 1 1 > test/$TASK.in.2e
+./gen 89712 20 1 1 7 > test/$TASK.in.2f
+./gen 89057 19 1 1 5 > test/$TASK.in.2g
+./gen 83495 20 1 1 3 > test/$TASK.in.2h
+./gen_spec 94239 18 1 1 > test/$TASK.in.2i
+./gen_spec 43241 19 1 1 > test/$TASK.in.2j
+./gen_spec 32134 20 1 1 > test/$TASK.in.2k
+./gen_spec 76532 20 1 1 > test/$TASK.in.2l
 
+# Subtask 3
+./gen 29834 50000 1 1 > test/$TASK.in.3a
+./gen 23389 120000 1 1 > test/$TASK.in.3b
+./gen 84989 150000 1 1 > test/$TASK.in.3c
+./gen 58489 180000 1 1 > test/$TASK.in.3d
+./gen 43423 200000 1 1 > test/$TASK.in.3e
+./gen 89712 200000 1 1 142431 > test/$TASK.in.3f
+./gen 89057 200000 1 1 > test/$TASK.in.3g
+./gen 83495 200000 1 1 1000000 > test/$TASK.in.3h
+./gen 89042 200000 1 1 > test/$TASK.in.3i
+./gen 54313 200000 1 1 > test/$TASK.in.3j
+./gen 84313 200000 1 1 > test/$TASK.in.3k
+./gen_spec 94239 180000 1 1 > test/$TASK.in.3l
+./gen_spec 43241 200000 1 1 > test/$TASK.in.3m
+./gen_spec 32134 200000 1 1 > test/$TASK.in.3n
+./gen_spec 76532 200000 1 1 > test/$TASK.in.3o
+./gen_spec 54323 200000 1 1 > test/$TASK.in.3p
+
+# Subtask 4
+./gen 29834 50000 50000 0 > test/$TASK.in.4a
+./gen 23389 120000 120000 0 > test/$TASK.in.4b
+./gen 84989 150000 200000 0 > test/$TASK.in.4c
+./gen 58489 180000 200000 0 > test/$TASK.in.4d
+./gen 43423 200000 123456 0 > test/$TASK.in.4e
+./gen 89712 200000 200000 0 142431 > test/$TASK.in.4f
+./gen 89057 200000 200000 0 > test/$TASK.in.4g
+./gen 83495 200000 200000 0 1000000 > test/$TASK.in.4h
+./gen 89042 200000 193176 0 > test/$TASK.in.4i
+./gen 54313 200000 200000 0 > test/$TASK.in.4j
+./gen 84313 200000 200000 0 > test/$TASK.in.4k
+./gen_spec 94239 180000 200000 0 > test/$TASK.in.4l
+./gen_spec 43241 200000 100000 0 > test/$TASK.in.4m
+./gen_spec 32134 200000 200000 0 > test/$TASK.in.4n
+./gen_spec 76532 200000 200000 0 > test/$TASK.in.4o
+./gen_spec 54323 200000 199999 0 > test/$TASK.in.4p
+
+# Subtask 5
+./gen 29834 50000 50000 1 > test/$TASK.in.5a
+./gen 23389 120000 120000 1 > test/$TASK.in.5b
+./gen 84989 150000 200000 1 > test/$TASK.in.5c
+./gen 58489 180000 200000 1 > test/$TASK.in.5d
+./gen 43423 200000 123456 1 > test/$TASK.in.5e
+./gen 89712 200000 200000 1 142431 > test/$TASK.in.5f
+./gen 89057 200000 200000 1 > test/$TASK.in.5g
+./gen 83495 200000 200000 1 1000000 > test/$TASK.in.5h
+./gen 89042 200000 193176 1 > test/$TASK.in.5i
+./gen 54313 200000 200000 1 > test/$TASK.in.5j
+./gen 84313 200000 200000 1 > test/$TASK.in.5k
+./gen_spec 94239 180000 200000 1 > test/$TASK.in.5l
+./gen_spec 43241 200000 100000 1 > test/$TASK.in.5m
+./gen_spec 32134 200000 200000 1 > test/$TASK.in.5n
+./gen_spec 76532 200000 200000 1 > test/$TASK.in.5o
+./gen_spec 54323 200000 199999 1 > test/$TASK.in.5p
